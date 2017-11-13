528aacf859e10d18b7a0e3e4a0b1b49b98bd6129
Takaya Kawano <takawano@microsoft.com>
2017-11-14 00:42:05 +0900

    Added this source codes

---
+++ voicetotext_0_0_0_0/new_Trrigerforspeechtotext.js
@@ -0,0 +1,17 @@
+function startrec() {
+    parent.Xrm.Page.getAttribute("description").setValue("");
+
+    client = Microsoft.CognitiveServices.SpeechRecognition.SpeechRecognitionServiceFactory.createMicrophoneClient(
+        0, //Recognition modes: Interactive mode, Conversation mode, Dictation mode
+        "en-us", // Recognition language
+        ""); // Please set YOUR_BING_SPEECH_API_KEY.
+
+    client.onFinalResponseReceived = function (response) {
+        parent.Xrm.Page.getAttribute("description").setValue(JSON.stringify(response[0].transcript));
+    }
+
+    client.startMicAndRecognition();
+    setTimeout(function () {
+        client.endMicAndRecognition();
+    }, 8000); // Please set the recording time(ms).
+}
